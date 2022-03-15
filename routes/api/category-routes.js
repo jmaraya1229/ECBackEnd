72f9e1dcfd627748131bdee58fbf8060a6b64655
jmaraya1229 <jessemaraya2013@gmail.com>
2022-03-14 21:03:44 -0700

    category routes works

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -36,15 +36,33 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new category
   try {
-    const categoryData = await Category.create(req.body);
+    const categoryData = await Category.create({
+      category_name: req.body.category_name
+    })
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    })
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+  res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
